21be7aa1e3ee531f982acfa47cecc89690526af8
Maryann Roshani <maryann.roshani@gmail.com>
2021-02-13 12:51:23 +1300

    second commit

---
+++ src/test/java/Demo1.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+
+
+   @Test
+    public void Demo() {
+//create driver object
+       System.out.println("hello");
+               /* System.setProperty("webdriver.chrome.driver","/Users/maryannroshani/Dev/ChromeDriver/chromedriver");
+                WebDriver driver = new ChromeDriver();
+                driver.get("http://google.com");
+                System.out.println(driver.getCurrentUrl());
+                System.out.println(driver.getTitle());
+                driver.get("http://yahoo.com");
+                driver.navigate().back();
+                System.out.println(driver.getTitle());
+                driver.close(); */
+    }
+
+    @Test
+    public void Demo2(){
+        System.out.println("bye");
+    }
+}
