9fee89855f3f9f9426fdfc9bdb554da3a130e0b1
maryannroshani <maryann.roshani@gmail.com>
2021-02-15 13:26:57 +1300

    Cucumber Framework

--- src/test/java/Demo1.java
+++ src/test/java/Demo1.java
@@ -1,15 +1,38 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Demo1 {
+ ExtentReports extent;
 
+ By Username = By.xpath("");
+
+ @BeforeTest
+           public void config(){
+    //ExtentReports, ExtentSparkReporter
+    String path = System.getProperty("user.dir") + "/target/index.html";
+
+    ExtentHtmlReporter reporter = new ExtentHtmlReporter(path);
+    reporter.config().setReportName("Automation Results");
+    reporter.config().setDocumentTitle("Test Results");
+    extent = new ExtentReports();
+    extent.attachReporter(reporter);
+    extent.setSystemInfo("Tester", "Maryann Roshani");
+   }
 
    @Test
     public void Demo() {
 //create driver object
-       System.out.println("hello");
-               /* System.setProperty("webdriver.chrome.driver","/Users/maryannroshani/Dev/ChromeDriver/chromedriver");
+       //System.out.println("hello");
+               ExtentTest test = extent.createTest("Demo");
+
+               System.setProperty("webdriver.chrome.driver","/Users/maryannroshani/Dev/ChromeDriver/chromedriver");
                 WebDriver driver = new ChromeDriver();
                 driver.get("http://google.com");
                 System.out.println(driver.getCurrentUrl());
@@ -17,12 +40,17 @@ public class Demo1 {
                 driver.get("http://yahoo.com");
                 driver.navigate().back();
                 System.out.println(driver.getTitle());
-                driver.close(); */
+                driver.close();
     }
 
     @Test
     public void Demo2(){
-        System.out.println("bye");
+        //System.out.println("bye");
 
+    }
+
+    @AfterTest
+    public void tearUp () {
+     extent.flush();
     }
 }
